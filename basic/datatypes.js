00488195c860a7e82d1d5f82cd92a9c0ee0dae2e
Ayushi Mishra <168451542+ayumishrag@users.noreply.github.com>
2025-07-05 08:27:25 +0000

    datatypes basic info done

---
+++ basic/datatypes.js
@@ -0,0 +1,21 @@
+'use strict' // when we write this in our code, this will always use the modern rules and version of JS.
+
+alert(3+3); //In browsers, alert is a built-in global function that shows popup dialogs. this will show reference error if we use it in node.
+console.log(alert); //popup will show only in browser.
+
+
+//datatypes 
+/*
+1- number = numeric vales 
+2- String = " " defines all the string values 
+3- Boolean = true/false 0 or 1 
+4- BigInt = used to define big integer values (used in trading and big companies like fb, twitter etc.)
+5- null = stand alone value 
+6- undefined = vale id not initialized 
+7- symbol = uniqueness 
+
+primitive	= Number, String, Boolean, Null, Undefined, BigInt, Symbol
+
+Non-Primitive =	Object, Array, Function, Date, RegExp, Map, Set, etc.
+8- Object = 
+*/
