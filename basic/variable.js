269937ea967da02cd416edfe71f9485ad5e97e27
Ayushi Mishra <168451542+ayumishrag@users.noreply.github.com>
2025-07-05 07:50:25 +0000

    basic variable done

---
+++ basic/variable.js
@@ -0,0 +1,10 @@
+const accountId = 12345;
+let name = "Ayushi";
+var accountemail = "ayu86@gmail.com";
+let password = "2345783598573";
+var city = "gurgaon";
+bankName = "SBO"; // without using any let,const or var we can initialize the variable in js. but this is not a good practice.
+let accountState; // while declaring the variable if we have not initialized thye v\alue in it, we get "undefined" in output. 
+console.log(accountId);
+console.table([ bankName, accountId, accountemail, name, password, city, accountState]);
+ 
